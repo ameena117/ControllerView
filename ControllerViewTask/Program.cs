11a02c762cc3deb050adeaf2a4e72411d5ecabbe
ameena117 <ameenajadallah@gmail.com>
2024-11-07 16:34:35 +0200

    update and clean

--- ControllerViewTask/Program.cs
+++ ControllerViewTask/Program.cs
@@ -14,7 +14,7 @@ namespace ControllerViewTask
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Shared/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -28,7 +28,7 @@ namespace ControllerViewTask
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Students}/{action=Index}/{id?}");
 
             app.Run();
         }
