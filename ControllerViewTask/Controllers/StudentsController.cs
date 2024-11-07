8627b9cb63ce6bcd99f8fc4980bb9152dd4dfe51
ameena117 <ameenajadallah@gmail.com>
2024-11-07 14:08:21 +0200

    Add project files.

---
+++ ControllerViewTask/Controllers/StudentsController.cs
@@ -0,0 +1,66 @@
+﻿using ControllerView.Data;
+using ControllerView.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerViewTask.Controllers
+{
+    public class StudentsController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            var allStudents = context.Students.ToList();
+            return View(allStudents);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student newStudent)
+        {
+            context.Students.Add(newStudent);
+            context.SaveChanges();
+            return RedirectToAction(nameof(GetAll));
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var originStudent = context.Students.Find(id);
+            return View(originStudent);
+        }
+        [HttpPost]
+        public IActionResult Update(Student currentStudent)
+        {
+            var originStudent = context.Students.Find(currentStudent.Id);
+            if (originStudent != null)
+            {
+                originStudent.FirstName = currentStudent.FirstName;
+                originStudent.LastName = currentStudent.LastName;
+                context.SaveChanges();
+                return RedirectToAction(nameof(GetAll));
+            }
+            return Ok("notfound");
+
+        }
+        public IActionResult Delete(int id)
+        {
+            var DeleteStudent = context.Students.Find(id);
+            if (DeleteStudent != null)
+                context.Students.Remove(DeleteStudent);
+            context.SaveChanges();
+            return RedirectToAction(nameof(GetAll));
+            return NotFound();
+        }
+
+
+    }
+}
---
+++ ControllerViewTask/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ControllerView.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControllerView.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ControllerViewTask;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
